b825bb73ff4bc931f094f57492c7ca0156bb4fb8
DanielFroehlich <daniel.froehlich@redhat.com>
2023-01-20 18:31:21 +0100

    Set useDynNFS correctly

--- getAllUsers.sh
+++ getAllUsers.sh
@@ -1,5 +1,5 @@
 
-for cluster in ocp1 ocp2 ocp3 ocp4 ocp5 ocp6 ocp7 ocp8 ocp9 ocp10 mmgt
+for cluster in ocp1 ocp2 ocp3 ocp4 ocp5 ocp6 ocp7 ocp8 ocp9 ocp10 ocp11 ocp12 mmgt
 do
   echo "Getting users for $cluster"
   oc login -u admin -p $PSWD --server=https://api.$cluster.stormshift.coe.muc.redhat.com:6443
