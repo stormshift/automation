b79b4cdf1b88e41328d0e51cc1f6a2cb297e9305
DanielFroehlich <daniel.froehlich@redhat.com>
2025-03-04 14:02:22 +0100

    Removed the RHV based stuff. We have it still on a branch, its not needed here any more

--- archived_RHVBased/downloadOCbinaries.sh
+++
@@ -1,7 +0,0 @@
-
-# OCP_VERSION=4.13
-curl https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/latest-$OCP_VERSION/openshift-install-linux.tar.gz  -o openshift-install-linux.tar.gz
-curl https://mirror.openshift.com/pub/openshift-v4/x86_64/clients/ocp/latest-$OCP_VERSION/openshift-client-linux.tar.gz  -o openshift-client-linux.tar.gz
-tar xfz openshift-install-linux.tar.gz
-tar xfz openshift-client-linux.tar.gz
-./oc version | grep Client
--- archived_RHVBased/rhacm/cleanup_rhacm_agent.sh
+++
@@ -1,3 +0,0 @@
-oc delete namespaces open-cluster-management-agent open-cluster-management-agent-addon --wait=false
-oc get crds | grep open-cluster-management.io | awk '{print $1}' | xargs oc delete crds --wait=false
-oc get crds | grep open-cluster-management.io | awk '{print $1}' | xargs oc patch crds --type=merge -p '{"metadata":{"finalizers": []}}'
